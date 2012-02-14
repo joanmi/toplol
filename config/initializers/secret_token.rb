b20bedce7eecd096f0ea92d1a5ce77092b72508a
joanmi <joanmi@imesmes.com>
2012-02-14 10:03:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toplol::Application.config.secret_token = 'edd53e5fe8c6c97d2e327c53e8e1c758fb6f09a1b4c3d428dfcae300610befcd9e0219febebaf2ea485d722de293fc7d02d997da33e51c95f30181cc958dbce2'
